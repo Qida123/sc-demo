c61de3d761e628579fd03d4cc370f97872509cc3
Qida123 <jingyi_bj@126.com>
2019-04-06 16:22:17 +0800

    initial commit

---
+++ consumer-ribbon/src/main/java/com/whzw/sc/HelloControler.java
@@ -0,0 +1,26 @@
+package com.whzw.sc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloControler {
+
+    @Autowired
+    HelloService helloService;
+
+    
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/hello/{name}")
+    public String hello(@PathVariable("name") String name) {
+        return port + ":" + helloService.hiService( name );
+    }
+    
+}
+
+
---
+++ producer/src/main/java/com/whzw/sc/HelloController.java
@@ -0,0 +1,22 @@
+package com.whzw.sc;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+    @Value("${server.port}")
+    String port; 
+	
+    @RequestMapping("/hello")
+    public String index(@RequestParam String name) {
+        return "hello "+name+"，this is message from "+port;
+    }
+}
+
+
+
+
