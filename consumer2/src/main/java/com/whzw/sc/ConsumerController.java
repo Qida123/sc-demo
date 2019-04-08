825547eab4c1ee5ba69f254a89a6104e641a2335
Qida123 <jingyi_bj@126.com>
2019-04-08 18:19:05 +0800

    hello

---
+++ consumer2/src/main/java/com/whzw/sc/ConsumerController.java
@@ -0,0 +1,24 @@
+package com.whzw.sc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConsumerController {
+
+    @Autowired
+    HelloRemote HelloRemote;
+    
+    @Value("${server.port}")
+    String port;
+	
+    @RequestMapping("/hello/{name}")
+    public String index(@PathVariable("name") String name) {
+        return port + ":" + HelloRemote.hello2(name);
+    }
+
+}
+
